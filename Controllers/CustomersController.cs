37e6799fdb27ae96ec36f89b6e5c669ed95a418e
CESAR <CESAR@SYSTEMGUSTAVO>
2024-08-17 21:22:03 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Examen01MVC_CesarJuñoFuentes.Models;
+
+namespace Examen01MVC_CesarJuñoFuentes.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private CodigoDBEntities db = new CodigoDBEntities();
+
+        // GET: Customers
+        public ActionResult Index()
+        {
+            //Mostrar Solo Activos
+            return View(db.Customers.ToList().Where(x=>x.IsActive==true).ToList());
+        }
+
+        // GET: Customers/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customers customers = db.Customers.Find(id);
+            if (customers == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customers);
+        }
+
+        // GET: Customers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Customers/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CustomerID,Name,DocumentNumber,DocumentType")] Customers customers)
+        {
+            if (ModelState.IsValid)
+            {
+                //poner en true;
+                customers.IsActive = true;
+                db.Customers.Add(customers);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(customers);
+        }
+
+        // GET: Customers/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customers customers = db.Customers.Find(id);
+            if (customers == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customers);
+        }
+
+        // POST: Customers/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CustomerID,Name,DocumentNumber,DocumentType")] Customers customers)
+        {
+            if (ModelState.IsValid)
+            {
+                //poner en true;
+                customers.IsActive = true;
+                db.Entry(customers).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(customers);
+        }
+
+        // GET: Customers/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customers customers = db.Customers.Find(id);
+            if (customers == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customers);
+        }
+
+        // POST: Customers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Customers customers = db.Customers.Find(id);
+            //Cambiar a False
+            customers.IsActive = false;
+            //Actualizar
+            db.Entry(customers).State = EntityState.Modified;
+            //db.Customers.Remove(customers);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult InactiveCustomers()
+        {
+            return View(db.Customers.ToList().Where(x => x.IsActive == false).ToList());
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Examen01MVC_CesarJuñoFuentes.MvcApplication" Language="C#" %>
